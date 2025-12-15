898ec73716b696b381d47a39b8a02bbf58a91654
Avishkar-5003 <tambeavishkar1@gmail.com>
2025-12-15 18:08:14 +0530

    Add files via upload
    Api calling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
-import Student from './components/Student'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos,setTodos] = useState([])
+   
+  const getData = () =>{
+     //https://dummyjson.com/todos
+
+    fetch('https://dummyjson.com/todos')
+    .then((rawData) => rawData.json())
+    .then((response) => setTodos(response.todos))
+    .catch((error) => console.log(error))
+  }
+
+  useEffect(() => {
+    getData();
+    
+  }, [])
+  
 
   return (
     <>
-      <Student></Student>
+    <table border={1}>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>todo</th>
+          <th>Completed</th>
+          <th>User Id</th>
+          
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          todos.map((rec) => (
+            <tr key={rec.id}>
+              <td>{rec.id}</td>
+              <td>{rec.todo}</td>
+              <td>{rec.completed? 'Yes' : 'No'}</td>
+              <td>{rec.userId}</td>
+            </tr>
+          ))
+        }
+
+      </tbody>
+    </table>
+      
     </>
   )
 }
